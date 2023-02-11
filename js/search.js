572c19e6980568a6d3d9740f2ef1f2aabffe317e
3eef8a28f26fb2bcc514e6f1938929a1f931762 <116031952+3eef8a28f26fb2bcc514e6f1938929a1f931762@users.noreply.github.com>
2023-02-11 18:10:30 -0500

    Typo, also forgot to push a file

---
+++ js/search.js
@@ -0,0 +1,65 @@
+// parses URL params
+const params = window
+	.location
+	.search
+	.slice(1)
+	.split("&")
+	.map(p => p.split("="))
+	.reduce((obj,[key,value]) =>
+		({ ...obj, [key]: decodeURIComponent(value) }),
+		{}
+	);
+
+const search_term = params["search"];
+
+const getSVG = function(name) {
+        return '<svg><use href="./icons.svg#'+name+'"></use></svg>';
+}
+
+const fuzzy_search_config = {
+        limit: 25,
+        includeScore: true,
+        ignoreLocation: true,
+        useExtendedSearch: true,
+        threshold: 0.01,
+        keys: [
+                {
+                        name: 'code',
+                        weight: 0.1
+                },
+                {
+                        name: 'description',
+                        weight: 0.1
+                },
+                {
+                        name: 'name',
+                        weight: 0.8
+                }
+        ]
+}
+
+const display_search_results = function(searchable_catalog) {
+        const fuse = new Fuse(searchable_catalog,fuzzy_search_config);
+        console.log("Searching for " + search_term + "...");
+        const results = fuse.search(search_term,{limit:fuzzy_search_config.limit});
+        const table = document.getElementById("searchResultsContainer");
+        results.forEach(function(search_entry) {
+                const entry = search_entry.item;
+                const tr = table.insertRow(-1);
+                tr.innerHTML += '<a href="courses/'
+                        + entry.code + '">'
+                        + '<h3><div><span class="courseName">'
+                        + entry.name
+                        + '</span><span class="courseCode">'
+                        + entry.code + "</span></div></h3>"
+                        + "<p>" + entry.description + "</p>" + '\n';
+        });
+}
+
+window.onload = function() {
+        // smart quotes
+        document.getElementById("searchTerm").innerHTML = "&#8220;" + search_term + "&#8221;";
+        fetch("searchable_catalog.json")
+                .then(r => r.json())
+                .then(display_search_results);
+}
