e29e1a146a7257ae96fd481f52dfe69d14702e24
3eef8a28f26fb2bcc514e6f1938929a1f931762 <116031952+3eef8a28f26fb2bcc514e6f1938929a1f931762@users.noreply.github.com>
2023-02-11 18:07:49 -0500

    Search is kinda working, and CSS works too

--- js/search_helper.js
+++ js/search_helper.js
@@ -1,40 +1,42 @@
 "use strict";
-const search_helper = function(event) {
+const search_helper = async function(event) {
         event.preventDefault();
         // "a     b   cde 12" => "a b cde 12"
         const input = document.getElementById("search").value.split(" ").join(" ");
-        console.log(input);
 
         var course_code = false;
         if(input.length == 8) {
                 // "abcd1345" => ["abcd","1345"] 
                 course_code = input.match(/.{1,4}/g)
-        } else if(input.length == 9) {
+        } else if(input.replace(/ |-/g,"").length == 8) {
                 // "abcd - 1345" => ["abcd","1345"]
-                course_code = input.split(/(?:-| )+/);
+                const arr = input.split(/(?:-| )+/);
+                if(arr.length == 2) course_code = arr;
         }
+
         // only do this logic if the string might be a course code
+        // avoid having to fetch the courses_list if it definitely isn't one
         if(course_code) {
                 // ["abcd","1345"] => "ABCD-1345"
                 course_code[0] = course_code[0].toUpperCase();
                 const code_str = course_code.join("-");
 
                 // check if "ABCD-1345" is a real course code
-                const course_exists = fetch("../courses_list.json")
+                const course_exists = await fetch("../courses_list.json")
                         .then(list => list.json())
-                        .then(list => code_str in list);
+                        .then(list => list.includes(code_str));
                 
                 // if it is, redirect to it
                 if(course_exists) {
-                        // handle both homepage and courses pages
+                        // handle both homepage and courses pages (which are in a directory)
                         if(window.location.pathname.split("/").slice(-2,-1)[0] != "courses") {
-                                location.href = "courses/"+code_str;
+                                location.href = "courses/"+code_str + ".html";
                         } else {
-                                location.href = code_str;
+                                location.href = code_str + ".html";
                         }
                         return;
                 }
         }
 
-        location.href = "../search?search=" + encodeURIComponent(input);
+        location.href = "../search" + ".html" + "?search=" + encodeURIComponent(input);
 }
